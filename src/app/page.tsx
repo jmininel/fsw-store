e9063b6bf8ec26e3ebf6e0a77949eac47b74828c
jmininel <ju.mininel@hotmail.com>
2023-10-17 17:11:15 -0300

    feat: created prisma schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <div className='text-red-500 bg-blue-500'></div>
+      <div className="text-red-500 bg-blue-500" ></div>
     </main>
   )
 }
